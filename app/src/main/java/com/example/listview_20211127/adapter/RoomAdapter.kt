fd2e628a08e6c916f83e6c342843ce8f143a067e
김창배 <cbkim93@naver.com>
2021-11-27 12:18:58 +0900

    Adapter Class 뼈대 작업

---
+++ app/src/main/java/com/example/listview_20211127/adapter/RoomAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.listview_20211127.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.listview_20211127.R
+import com.example.listview_20211127.data.RoomData
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomData>
+) : ArrayAdapter<RoomData>(mContext,resId,mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if(tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+
+        return row
+    }
+}
